9ef23d2f79558ab8cf9d8c36b2f15bb73e88d741
Erich Json Fosse <erich@halloverden.no>
2020-01-13 19:05:52 +0100

    ADD: ZSH config file

---
+++ config/zsh-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Configuring ZSH..."
+
+# Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Oh My ZSH Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Oh My ZSH Theme (Powerlevel9k)
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
