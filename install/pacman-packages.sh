3fdf0517e62ab444223aacd341aad2dde0192ed9
Erich Json Fosse <erich@halloverden.no>
2020-01-15 00:22:06 +0100

    ADD: --noconfirm for pacman

--- install/pacman-packages.sh
+++ install/pacman-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Installing pacman packages..."
-pacman -Syu
-pacman -Syy chromium curl docker docker-compose filezilla konsole libreoffice-fresh pamac powerline-fonts snapd ufw unzip yakuake zsh
+pacman -Syu --noconfirm
+pacman -Sy --noconfirm chromium curl docker docker-compose filezilla konsole libreoffice-fresh pamac powerline-fonts snapd ufw unzip yakuake zsh
