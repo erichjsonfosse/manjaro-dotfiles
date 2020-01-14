bceea404ce7ab3a5ecb0b305b27a6426102950b1
Erich Json Fosse <erich@halloverden.no>
2020-01-15 00:17:21 +0100

    ADD: Sourcing scripts

--- init.sh
+++ init.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Running init..."
+echo "Installing..."
+
+chmod +x ./install/*.sh
+chmod +x ./config/*.sh
+
+source ./install/pacman-packages.sh
+source ./install/snap-packages.sh
+source ./install/aur-packages.sh
+
+source ./configure/ufw-config.sh
+source ./configure/zsh-config.sh
+
+# TODO: Support upgrades
