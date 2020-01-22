6c293e4c83b30c5a7b7d752a84b7cc49f941328c
Erich Json Fosse <erich@halloverden.no>
2020-01-22 21:36:31 +0100

    ADD: .zshrc config

--- config/zsh-config.sh
+++ config/zsh-config.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-echo "Configuring ZSH..."
+echo "Configuring Zsh with Oh My Zsh..."
 
-# Oh My ZSH
+# Oh My Zsh
 curl -fsSL -o install-ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 chmod +x install-ohmyzsh.sh
-# Oh My ZSH install options
-CHSH="yes"
-RUNZSH="no"
-./install-ohmyzsh.sh
+# Oh My Zsh install options
+./install-ohmyzsh.sh --unattended
 rm -f ./install-ohmyzsh.sh
 
-# Oh My ZSH Plugins
+# Oh My Zsh Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-# Oh My ZSH Theme (Powerlevel9k)
+# Oh My Zsh Theme (Powerlevel9k)
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Activate template
+sed -i -e "s/^ZSH_THEME=(.*)/#ZSH_THEME=$1/g" ~/.zshrc
+sed -i -e "s/^#ZSH_THEME=.*/ZSH_THEME=\"powerlevel9k/powerlevel9k\"/g" ~/.zshrc
+
+# Activate plugins
+sed -i -e "s/^plugins=(.*)/#plugins=$1/g" ~/.zshrc
+sed -i -e "s/^#plugins=.*/plugins=\(git zsh-autosuggestions zsh-syntax-highlighting\)/g" ~/.zshrc
+
+# Activate aliases
+sed -i -e "\$aTEXTTOEND" ~/.zshrc
