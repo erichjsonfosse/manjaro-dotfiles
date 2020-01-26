f89ed408171ccbc10fc0115eda53a41cfa079394
Erich Json Fosse <erich@halloverden.no>
2020-01-27 00:42:56 +0100

    ADD: Installing squashfuse

--- install/pacman-packages.sh
+++ install/pacman-packages.sh
@@ -2,4 +2,4 @@
 
 echo "Installing pacman packages..."
 sudo pacman -Syu --noconfirm
-sudo pacman -Sy --noconfirm chromium curl docker docker-compose filezilla konsole libreoffice-fresh otf-fira-code pamac powerline-fonts snapd ufw unzip yakuake zsh
+sudo pacman -Sy --noconfirm chromium curl docker docker-compose filezilla konsole libreoffice-fresh otf-fira-code pamac powerline-fonts snapd squashfuse ufw unzip yakuake zsh
