86a52d402b254cf0c1d3faeadae6c1baf90e5ca4
Erich Json Fosse <erich@halloverden.no>
2020-01-26 20:57:40 +0100

    ADD: Script for configuring Git

---
+++ config/git-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+GITCONFIGFILE="$HOME/.gitconfig"
+
+# Add .gitconfig
+cp -f "$BASEDIR/.gitconfic.template" "$GITCONFIGFILE"
+
+# Replace name and email
+sed -i "s/MANJARO_DOTFILES_GITCONFIG_NAME/$MANJARO_DOTFILES_GITCONFIG_NAME/g" "$GITCONFIGFILE"
+sed -i "s/MANJARO_DOTFILES_GITCONFIG_EMAIL/$MANJARO_DOTFILES_GITCONFIG_EMAIL/g" "$GITCONFIGFILE"
