b07cb318bd543b93a71b558bddf66a66de7de9f2
Erich Json Fosse <erich@halloverden.no>
2020-01-26 20:56:53 +0100

    ADD: Installing fira code fonts

--- install/pacman-packages.sh
+++ install/pacman-packages.sh
@@ -2,4 +2,4 @@
 
 echo "Installing pacman packages..."
 pacman -Syu --noconfirm
-pacman -Sy --noconfirm chromium curl docker docker-compose filezilla konsole libreoffice-fresh pamac powerline-fonts snapd ufw unzip yakuake zsh
+pacman -Sy --noconfirm chromium curl docker docker-compose filezilla konsole libreoffice-fresh otf-fira-code pamac powerline-fonts snapd ufw unzip yakuake zsh
