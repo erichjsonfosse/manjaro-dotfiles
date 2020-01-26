cb05c522d96a1bb6a0ccb6bf33a9cefa8a8a81e5
Erich Json Fosse <erich@halloverden.no>
2020-01-26 20:57:09 +0100

    ADD: Script for requesting user input

---
+++ request-input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+######################### Git Config ##########################
+read -rp 'Git Config Name: ' MANJARO_DOTFILES_GITCONFIG_NAME
+read -rp 'Git Config Email: ' MANJARO_DOTFILES_GITCONFIG_EMAIL
+
+export MANJARO_DOTFILES_GITCONFIG_NAME
+export MANJARO_DOTFILES_GITCONFIG_EMAIL
+######################### Git Config ##########################
