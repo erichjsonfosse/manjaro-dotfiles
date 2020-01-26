f7407309a58c5a91856cde38b6a08ca6d842e915
Erich Json Fosse <erich@halloverden.no>
2020-01-26 20:58:05 +0100

    ADD: Running scripts for requesting user input, and for configuring Git

--- init.sh
+++ init.sh
@@ -5,11 +5,13 @@ echo "Installing..."
 chmod +x ./install/*.sh
 chmod +x ./config/*.sh
 
+source ./request-input.sh
 source ./install/pacman-packages.sh
 source ./install/snap-packages.sh
 source ./install/aur-packages.sh
 
 source ./config/ufw-config.sh
 source ./config/zsh-config.sh
+source ./config/git-config.sh
 
 # TODO: Support upgrades
