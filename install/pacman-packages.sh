fed9cb49a446727e147e3e9dc0c851c619987eae
Erich Json Fosse <erich@halloverden.no>
2020-02-17 22:51:08 +0100

    EDIT: Rearranged packages for readability

--- install/pacman-packages.sh
+++ install/pacman-packages.sh
@@ -4,4 +4,25 @@ echo "Upgrading pacman packages..."
 pacman -Syu --noconfirm
 
 echo "Installing pacman packages..."
-pacman -Sy --noconfirm chromium curl docker docker-compose filezilla konsole libreoffice-fresh otf-fira-code pamac powerline-fonts snapd squashfuse ufw unzip yakuake zsh
+pacman -Sy --noconfirm \
+ `# Browsers` \
+ chromium \
+ `# Development` \
+ docker \
+ docker-compose \
+ libreoffice-fresh \
+ `# Fonts` \
+ otf-fira-code \
+ powerline-fonts \
+ ttf-fira-code \
+ `# Utilities` \
+ curl \
+ filezilla \
+ konsole \
+ pamac \
+ snapd \
+ squashfuse \
+ ufw \
+ unzip \
+ yakuake \
+ zsh
