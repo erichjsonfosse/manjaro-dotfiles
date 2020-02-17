093641f155307e2f782a7e68537355e486dec553
Erich Json Fosse <erich@halloverden.no>
2020-02-18 00:25:20 +0100

    ADD: Generating SSH key files if user wants it

--- request-input.sh
+++ request-input.sh
@@ -7,3 +7,14 @@ read -rp 'Git Config Email: ' MANJARO_DOTFILES_GITCONFIG_EMAIL
 export MANJARO_DOTFILES_GITCONFIG_NAME
 export MANJARO_DOTFILES_GITCONFIG_EMAIL
 ######################### Git Config ##########################
+
+######################### SSH key files ##########################
+while true; do
+  read -rp "Generate SSH key files? (y/n)" yn
+  case $yn in
+    [Yy]* ) ssh-keygen;;
+    [Nn]* ) break;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+######################### SSH key files ##########################
