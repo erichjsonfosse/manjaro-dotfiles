2b6821a8ace5e4e54f1b21627fa3fb2c6ab0e35f
Erich Json Fosse <erich@halloverden.no>
2020-11-16 15:30:25 +0100

    EDIT: Put URLs in quotes

--- install/app-images.sh
+++ install/app-images.sh
@@ -8,8 +8,8 @@ APPIMAGEDIR="/opt/app-images"
 mkdir -p "$APPIMAGEDIR"
 
 # Insomnia Designer
-wget -O "$APPIMAGEDIR/insomnia-designer" https://updates.insomnia.rest/downloads/linux/latest?app=com.insomnia.designer
+wget -O "$APPIMAGEDIR/insomnia-designer" "https://updates.insomnia.rest/downloads/linux/latest?app=com.insomnia.designer"
 # Insomnia REST Client
-wget -O "$APPIMAGEDIR/insomnia-rest-client" https://updates.insomnia.rest/downloads/linux/latest
+wget -O "$APPIMAGEDIR/insomnia-rest-client" "https://updates.insomnia.rest/downloads/linux/latest"
 # Rambox
-wget -O "$APPIMAGEDIR/rambox" https://rambox.pro/api/downloadCE?os=linux&filetype=AppImage&arch=x86_64
+wget -O "$APPIMAGEDIR/rambox" "https://rambox.pro/api/downloadCE?os=linux&filetype=AppImage&arch=x86_64"
