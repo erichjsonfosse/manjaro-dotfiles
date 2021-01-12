618ee4ab3788cd6c2013b23ea92b178d74309641
Erich Json Fosse <erich@halloverden.no>
2021-01-12 01:06:55 +0100

    ADD: Functions and steps

--- init.sh
+++ init.sh
@@ -9,33 +9,165 @@ fi
 while true; do
   read -rp "Continue with installation? (y/n)" yn
   case $yn in
-    [Yy]* ) break;;
+    [Yy]* ) doRun(); break;;
     [Nn]* ) exit;;
     * ) echo "Please answer yes or no.";;
   esac
 done
 
-chmod +x ./*.sh
-chmod +x ./install/*.sh
-chmod +x ./config/*.sh
+doRun()
+{
+  filename="RESUME.tmp"
+  if [ -f "$filename" ]; then
+    step=$(head -n 1 $filename);
+  fi
+  
+  chmodScripts;
+  
+  if [ $step lt 2 ]; then
+    setVariables;
+  fi
+  
+  if [ $step lt 3 ]; then
+    requestInput;
+  fi
+  
+  if [ $step lt 4 ]; then
+    installPackmanPackages;
+  fi
+  
+  if [ $step lt 5 ]; then
+    enableSnapService;
+  fi
+  
+  if [ $step lt 6 ]; then
+    installSnapPackages;
+  fi
+  
+  if [ $step lt 7 ]; then
+    installAurPackages;
+  fi
+  
+  if [ $step lt 8 ]; then
+    installAppImages;
+  fi
+  
+  if [ $step lt 9 ]; then
+    configureUfw;
+  fi
+  
+  if [ $step lt 10 ]; then
+    configureZsh;
+  fi
+  
+  if [ $step lt 11 ]; then
+    configureGit;
+  fi
+  
+  if [ $step lt 12 ]; then
+    configureSsh;
+  fi
+  
+  if [ $step lt 13 ]; then
+    bumpVersion;
+  fi
+  
+  if [ $step lt 14 ]; then
+    configureSsh;
+  fi
+}
 
-source ./set-variables.sh
-source ./request-input.sh
-source ./install/pacman-packages.sh
-source ./install/enable-snap-service.sh
-# TODO: Handle session restart after enabling snap service
-source ./install/snap-packages.sh
-# source ./install/aur-packages.sh
-source ./install/app-images.sh
+setStep()
+{
+  filename="RESUME.tmp";
+  rm $filename;
+  echo $1 > $filename;
+}
 
-source ./config/ufw-config.sh
-source ./config/zsh-config.sh
-source ./config/git-config.sh
-source ./config/ssh-config.sh
+chmodScripts()
+{
+  setStep 1;
+  chmod +x ./*.sh
+  chmod +x ./install/*.sh
+  chmod +x ./config/*.sh
+}
 
-source ./bump-version.sh
+setVariables()
+{
+  setStep 2;
+  source ./set-variables.sh
+}
 
+requestInput()
+{
+  setStep 3;
+  source ./request-input.sh
+}
 
-# Change ownership of home folder files recursively
-echo "Changing ownership of home folder..."
-chown -R "$LOGNAME:$LOGNAME" "$HOMEDIR"
+installPackmanPackages()
+{
+  setStep 4;
+  source ./install/pacman-packages.sh
+}
+
+enableSnapService()
+{
+  setStep 5;
+  source ./install/enable-snap-service.sh
+}
+
+installSnapPackages()
+{
+  setStep 6;
+  source ./install/snap-packages.sh
+}
+
+installAurPackages()
+{
+  setStep 7;
+#  source ./install/aur-packages.sh
+}
+
+installAppImages()
+{
+  setStep 8;
+  source ./install/app-images.sh
+}
+
+configureUfw()
+{
+  setStep 9;
+  source ./config/ufw-config.sh
+}
+
+configureZsh()
+{
+  setStep 10;
+  source ./config/zsh-config.sh
+}
+
+configureGit()
+{
+  setStep 11;
+  source ./config/git-config.sh
+}
+
+configureSsh()
+{
+  setStep 12;
+  source ./config/ssh-config.sh
+}
+
+bumpVersion()
+{
+  setStep 13;
+  source ./bump-version.sh
+}
+
+configureSsh()
+{
+  setStep 14;
+  # Change ownership of home folder files recursively
+  echo "Changing ownership of home folder..."
+  chown -R "$LOGNAME:$LOGNAME" "$HOMEDIR"
+}
