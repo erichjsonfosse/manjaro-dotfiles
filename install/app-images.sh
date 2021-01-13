77dc8fb1e03e776147dae3b927329f9f78312747
Erich Json Fosse <erich@halloverden.no>
2021-01-13 21:30:50 +0100

    EDIT: No longer fetching any app images

--- install/app-images.sh
+++ install/app-images.sh
@@ -6,11 +6,11 @@ echo "Installing App Images..."
 mkdir -p "$APP_IMAGE_DIR"
 
 # Atom Editor
-wget -O "$APP_IMAGE_DIR/atom-editor.AppImage" "https://bintray.com/probono/AppImages/download_file?file_path=Atom-v1.18.0.glibc2.17-x86_64.AppImage"
+# wget -O "$APP_IMAGE_DIR/atom-editor.AppImage" "https://bintray.com/probono/AppImages/download_file?file_path=Atom-v1.18.0.glibc2.17-x86_64.AppImage"
 # Insomnia Designer
-wget -O "$APP_IMAGE_DIR/insomnia-designer.AppImage" "https://updates.insomnia.rest/downloads/linux/latest?app=com.insomnia.designer"
+# wget -O "$APP_IMAGE_DIR/insomnia-designer.AppImage" "https://updates.insomnia.rest/downloads/linux/latest?app=com.insomnia.designer"
 # Insomnia REST Client
-wget -O "$APP_IMAGE_DIR/insomnia-rest-client.AppImage" "https://updates.insomnia.rest/downloads/linux/latest"
+# wget -O "$APP_IMAGE_DIR/insomnia-rest-client.AppImage" "https://updates.insomnia.rest/downloads/linux/latest"
 # Rambox
 # wget -O "$APP_IMAGE_DIR/rambox.AppImage" "https://rambox.pro/api/downloadCE?os=linux&filetype=AppImage&arch=x86_64"
 
