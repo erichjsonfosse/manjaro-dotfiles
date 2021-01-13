c7292242c686dd7a3cb511d299c297e9277c420a
Erich Json Fosse <erich@halloverden.no>
2021-01-13 21:31:14 +0100

    ADD: Installing Rambox, Insomnia, Insomnia Designer and Atom

--- install/snap-packages.sh
+++ install/snap-packages.sh
@@ -4,3 +4,7 @@ echo "Installing Snap packages..."
 
 snap install intellij-idea-ultimate --classic
 snap install postman
+snap install rambox
+snap install insomnia
+snap install insomnia-designer
+snap install atom --classic
