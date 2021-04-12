18e75b914b041054726b7b9544540c00f8bd1d17
Erich Json Fosse <erich@halloverden.no>
2021-04-12 12:41:52 +0200

    EDIT: Installing Postman v8

--- install/snap-packages.sh
+++ install/snap-packages.sh
@@ -3,7 +3,7 @@
 echo "Installing Snap packages..."
 
 snap install intellij-idea-ultimate --classic
-snap install postman
+snap install postman --channel=v8/stable
 snap install rambox
 snap install insomnia
 snap install insomnia-designer
