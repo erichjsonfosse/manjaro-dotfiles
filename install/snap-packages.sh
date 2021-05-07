0dc4a15e324ca6c8f263f4456aa68ead821b708c
Erich Json Fosse <erich@halloverden.no>
2021-05-08 00:11:33 +0200

    ADD: Installing Rider

--- install/snap-packages.sh
+++ install/snap-packages.sh
@@ -3,6 +3,7 @@
 echo "Installing Snap packages..."
 
 snap install intellij-idea-ultimate --classic
+snap install rider --classic
 snap install postman --channel=v8/stable
 snap install rambox
 snap install insomnia
