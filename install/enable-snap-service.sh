ae1f4a929663cb60e6f5761adbe7470365f80ac1
Erich Json Fosse <erich@halloverden.no>
2023-03-04 15:16:40 +0100

    Update enable-snap-service.sh

--- install/enable-snap-service.sh
+++ install/enable-snap-service.sh
@@ -6,12 +6,4 @@ echo "Enabling snap service..."
 systemctl enable --now snapd.socket
 systemctl enable --now snapd.service
 
-while true; do
-  read -rp "Reboot (recommended)? (y/n)" yn
-  case $yn in
-    [Yy]* ) reboot; exit;;
-    [Nn]* ) break;;
-    * ) echo "Please answer yes or no.";;
-  esac
-done
-
+askForReboot
