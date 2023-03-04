692e348a3cfc28aaae7fbd0612b230439324c999
Erich Json Fosse <erich@halloverden.no>
2023-03-04 22:00:04 +0100

    Update request-input.sh

--- request-input.sh
+++ request-input.sh
@@ -17,7 +17,7 @@ mkdir -p "$HOMEDIR/.ssh"
 while true; do
   read -rp "Generate SSH key files? (y/n)" yn
   case $yn in
-    [Yy]* ) ssh-keygen -f "$HOMEDIR/.ssh/id_rsa" && chown "$LOGNAME":"$LOGNAME" "$HOMEDIR/.ssh/id_rsa" && break;;
+    [Yy]* ) ssh-keygen -t ed25519 -f "$HOMEDIR/.ssh/id_rsa" && chown "$LOGNAME":"$LOGNAME" "$HOMEDIR/.ssh/id_rsa" && break;;
     [Nn]* ) break;;
     * ) echo "Please answer yes or no.";;
   esac
