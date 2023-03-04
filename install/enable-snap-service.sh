1ee4d566ebecce50a5c372a0d11b61f972466abd
Erich Json Fosse <erich@halloverden.no>
2023-03-04 21:22:41 +0100

    Update enable-snap-service.sh

--- install/enable-snap-service.sh
+++ install/enable-snap-service.sh
@@ -5,3 +5,6 @@ echo "Enabling snap service..."
 # Enabling Service
 systemctl enable --now snapd.socket
 systemctl enable --now snapd.service
+
+waitUntilServiceIsRunning "snapd.socket"
+waitUntilServiceIsRunning "snapd.service"
