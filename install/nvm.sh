deff6cf10dac7f4fbfeb5787d904300231530376
Erich Json Fosse <erich@halloverden.no>
2023-06-02 01:49:43 +0200

    Update nvm.sh
    No longer running su for latest nvm version

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,2 +1,2 @@
-latestNvmVersion=$(su "$LOGNAME" -c 'getlatesttag https://github.com/nvm-sh/nvm')
+latestNvmVersion=$(getlatesttag https://github.com/nvm-sh/nvm)
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$latestNvmVersion/install.sh" | bash
