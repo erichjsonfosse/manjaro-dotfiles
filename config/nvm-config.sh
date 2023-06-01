07ff60abcb4d311344c124da0fe8d5c40c4d71a9
Erich Json Fosse <erich@halloverden.no>
2023-06-01 21:14:31 +0200

    Update nvm-config.sh
    Fixed the nvm config file

--- config/nvm-config.sh
+++ config/nvm-config.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "\n\nexport NVM_DIR=\"$([ -z \"${XDG_CONFIG_HOME-}\" ] && printf %s \"${HOME}/.nvm\" || printf %s \"${XDG_CONFIG_HOME}/nvm\")\"\n[ -s \"$NVM_DIR/nvm.sh\" ] && \\. \"$NVM_DIR/nvm.sh\" # This loads nvm" >> "$ZSHRCFILE"
+printf "\n\n" >> "$ZSHRCFILE"
+printf "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %%s \"\${HOME}/.nvm\" || printf %%s \"\${XDG_CONFIG_HOME}/nvm\")\"" >> "$ZSHRCFILE"
+printf "\n" >> "$ZSHRCFILE"
+printf "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" # This loads nvm" >> "$ZSHRCFILE"
