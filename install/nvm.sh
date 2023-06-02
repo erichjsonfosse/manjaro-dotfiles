2c406e52ffdf72d91240aebb40b0fb19921cc762
Erich Json Fosse <erich@halloverden.no>
2023-06-02 02:35:27 +0200

    Update nvm.sh

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,3 +1,3 @@
 latestNvmVersion=$(getlatesttag https://github.com/nvm-sh/nvm)
 curl -fsSL -o install-nvm.sh "https://raw.githubusercontent.com/nvm-sh/nvm/$latestNvmVersion/install.sh"
-su "$LOGNAME" -c 'install-nvm.sh'
+su "$LOGNAME" -c './install-nvm.sh'
