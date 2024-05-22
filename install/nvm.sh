a36195688318dd7bbab29b5830fdb78cf8e24a59
Erich Json Fosse <erich@halloverden.no>
2024-05-22 15:06:24 +0200

    Delete install/nvm.sh

--- install/nvm.sh
+++
@@ -1,4 +0,0 @@
-latestNvmVersion=$(getlatesttag https://github.com/nvm-sh/nvm)
-curl -fsSL -o install-nvm.sh "https://raw.githubusercontent.com/nvm-sh/nvm/$latestNvmVersion/install.sh"
-chmod +x install-nvm.sh
-su "$LOGNAME" -c './install-nvm.sh'
