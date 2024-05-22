f75e47c9508a336d42fdbf08b4396ebbc78bb009
Erich Json Fosse <erich@halloverden.no>
2024-05-22 15:06:08 +0200

    Update nvm-config.sh

--- config/nvm-config.sh
+++ config/nvm-config.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 printf "\n\n" >> "$ZSHRCFILE"
-printf "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %%s \"\${HOME}/.nvm\" || printf %%s \"\${XDG_CONFIG_HOME}/nvm\")\"" >> "$ZSHRCFILE"
+printf "[ -z \"$NVM_DIR\" ] && export NVM_DIR=\"$HOME/.nvm\"" >> "$ZSHRCFILE"
 printf "\n" >> "$ZSHRCFILE"
-printf "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" # This loads nvm" >> "$ZSHRCFILE"
+printf "source /usr/share/nvm/nvm.sh" >> "$ZSHRCFILE"
+printf "\n" >> "$ZSHRCFILE"
+printf "source /usr/share/nvm/bash_completion" >> "$ZSHRCFILE"
+printf "\n" >> "$ZSHRCFILE"
+printf "source /usr/share/nvm/install-nvm-exec" >> "$ZSHRCFILE"
+printf "\n" >> "$ZSHRCFILE"
